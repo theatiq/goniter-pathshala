cb67c7be26afe372b19cf8f4eb5251b43958e93d
theatiq <theatiq@gmail.com>
2025-07-22 10:51:01 +0600

    events added

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -1,7 +1,8 @@
 "use client";
-import 'react-calendar/dist/Calendar.css';
+import "react-calendar/dist/Calendar.css";
 import { useState } from "react";
 import Calendar from "react-calendar";
+import Image from "next/image";
 
 type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
@@ -28,12 +29,29 @@ const events = [
   },
 ];
 
-
 const EventCalendar = () => {
   const [value, onChange] = useState<Value>(new Date());
   return (
     <div className="bg-white p-4 rounded">
       <Calendar onChange={onChange} value={value} />
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-semibold my-4">Events</h1>
+        <Image src={"/moreDark.png"} alt="more" width={20} height={20} />
+      </div>
+      <div className="flex flex-col gap-4">
+        {events.map((event) => (
+          <div
+            className="p-5 rounded-md border-2 border-gray-100 border-t-4 odd:border-lamaSky even:border-lamaPurple"
+            key={event.id}
+          >
+            <div className="flex items-center justify-between">
+              <h1 className="font-semibold text-gray-600">{event.title}</h1>
+              <span className="text-gray-300 text-xs">{event.time}</span>
+            </div>
+            <p className="mt-2 text-gray-400 text-sm">{event.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
