3370a129f181193cadb7fb6769f083f6e454783f
theatiq <theatiq@gmail.com>
2025-07-22 17:21:08 +0600

    Teacher Page added

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,9 +1,25 @@
+import Announcement from "@/components/Announcement";
+import BigCalendar from "@/components/BigCalendar";
+import EventCalendar from "@/components/EventCalendar";
 import React from "react";
 
 const TeacherPage = () => {
-  return <div>
-    Teachers
-  </div>;
+  return (
+    <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+      {/* Left */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white rounded-md p-4">
+          <h1 className="text-xl font-semibold">Schedule(4A)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* Right */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcement />
+      </div>
+    </div>
+  );
 };
 
 export default TeacherPage;
