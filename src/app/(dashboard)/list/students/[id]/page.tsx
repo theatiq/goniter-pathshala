d5609cc75f908d3b802fc33d31fc2130edf638d4
theatiq <theatiq@gmail.com>
2025-07-26 15:45:24 +0600

    Single Student added

---
+++ src/app/(dashboard)/list/students/[id]/page.tsx
@@ -0,0 +1,147 @@
+import Announcement from "@/components/Announcement";
+import BigCalendar from "@/components/BigCalendar";
+import Performance from "@/components/Performance";
+import Image from "next/image";
+import Link from "next/link";
+
+const SingleStudentPage = () => {
+  return (
+    <div className="flex-1 p-4 flex flex-col gap-4 xl:flex-row">
+      {/* Left */}
+      <div className="w-full xl:w-2/3">
+        {/* Top */}
+        <div className="flex flex-col lg:flex-row gap-4">
+          {/* User Info Card */}
+          <div className="bg-lamaSky py-6 px-4 rounded-md flex-1 flex gap-4">
+            <div className="w-1/3">
+              <Image
+                src={
+                  "https://images.pexels.com/photos/2888150/pexels-photo-2888150.jpeg?auto=compress&cs=tinysrgb&w=1200"
+                }
+                alt=""
+                width={144}
+                height={144}
+                className="w-36 h-36 rounded-full object-cover"
+              />
+            </div>
+            <div className="w-2/3">
+              <h1 className="text-xl font-semibold">John Doe</h1>
+              <p className="text-sm text-gray-500">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              </p>
+              <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src={"/blood.png"} alt="" width={14} height={14} />
+                  <span>O+</span>
+                </div>
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src={"/date.png"} alt="" width={14} height={14} />
+                  <span>01-01-25</span>
+                </div>
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src={"/mail.png"} alt="" width={14} height={14} />
+                  <span>theatiq@gmail.com</span>
+                </div>
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src={"/phone.png"} alt="" width={14} height={14} />
+                  <span>01234568</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Small Cards */}
+          <div className="flex-1 flex justify-between gap-4 flex-wrap">
+            {/* Card */}
+            <div className="bg-white rounded-md p-4 flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+              <Image
+                src={"/singleAttendance.png"}
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div>
+                <h1 className="text-xl font-semibold">95%</h1>
+                <span className="text-xs text-gray-500">Attendance</span>
+              </div>
+            </div>
+            {/* Card */}
+            <div className="bg-white rounded-md p-4 flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+              <Image
+                src={"/singleLesson.png"}
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div>
+                <h1 className="text-xl font-semibold">6</h1>
+                <span className="text-xs text-gray-500">Lessons</span>
+              </div>
+            </div>
+            {/* Card */}
+            <div className="bg-white rounded-md p-4 flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+              <Image
+                src={"/singleBranch.png"}
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div>
+                <h1 className="text-xl font-semibold">2</h1>
+                <span className="text-xs text-gray-500">Branches</span>
+              </div>
+            </div>
+            {/* Card */}
+            <div className="bg-white rounded-md p-4 flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+              <Image
+                src={"/singleClass.png"}
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div>
+                <h1 className="text-xl font-semibold">5</h1>
+                <span className="text-xs text-gray-500">Classes</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Bottom */}
+        <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
+          <h1>Student&apos;s Schedule</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* Right */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-4">
+        <div className="bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Shortcuts</h1>
+          <div className="mt-4 flex flex-wrap gap-4 text-xs text-gray-500">
+            <Link className="p-3 rounded-md bg-lamaSkyLight" href={"/"}>
+              Student&apos;s Classes
+            </Link>
+            <Link className="p-3 rounded-md bg-lamaPurpleLight" href={"/"}>
+              Student&apos;s Teachers
+            </Link>
+            <Link className="p-3 rounded-md bg-lamaYellowLight" href={"/"}>
+              Student&apos;s Results
+            </Link>
+            <Link className="p-3 rounded-md bg-pink-50" href={"/"}>
+              Student&apos;s Exams
+            </Link>
+            <Link className="p-3 rounded-md bg-lamaSkyLight" href={"/"}>
+              Student&apos;s Assignments
+            </Link>
+          </div>
+        </div>
+        <Performance />
+        <Announcement />
+      </div>
+    </div>
+  );
+};
+
+export default SingleStudentPage;
--- src/components/Performance.tsx
+++ src/components/Performance.tsx
@@ -9,13 +9,13 @@ const data = [
 
 const Performance = () => {
   return (
-    <div className="bg-white rounded-md p-4 h-80">
+    <div className="bg-white rounded-md p-4 h-80 relative">
       <div className="flex items-center justify-between">
         <h1 className="text-xl font-semibold">Performances</h1>
         <Image src={"/moreDark.png"} alt="" width={16} height={16} />
       </div>
       <ResponsiveContainer width="100%" height="100%">
-        <PieChart width={400} height={400}>
+        <PieChart>
           <Pie
             dataKey="value"
             startAngle={180}
@@ -23,12 +23,18 @@ const Performance = () => {
             data={data}
             cx="50%"
             cy="50%"
-            outerRadius={80}
+            innerRadius={70}
             fill="#8884d8"
-            label
           />
         </PieChart>
       </ResponsiveContainer>
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+        <h1 className="text-3xl font-bold">9.2</h1>
+        <p className="text-xs text-gray-300">of 10 Max LTS</p>
+      </div>
+      <h2 className="font-medium absolute bottom-16 left-0 right-0 m-auto text-center">
+        1st Semester - 2nd Semester
+      </h2>
     </div>
   );
 };
